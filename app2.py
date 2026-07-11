6b617ccecfdcab6ae4e4e983e42b7bc49571d011
Raghotham <psraghothamrao8@gmail.com>
2026-07-11 21:31:55 +0530

    temp changes

---
+++ app2.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # os.environ.get reads the secret injected by Dockers
+    secret = os.environ.get('SECRET_KEY', 'no-key-found')
+    return f"Hello! {secret} is the secret of my energy!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
